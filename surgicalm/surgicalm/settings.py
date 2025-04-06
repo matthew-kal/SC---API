d349dcd56957b87e8d1c0dcb5a9219fe24f41c9a
matthew-kal <matthewkalender@gmail.com>
2025-04-06 00:36:25 -0400

    Minor change

--- surgicalm/surgicalm/settings.py
+++ surgicalm/surgicalm/settings.py
@@ -150,7 +150,8 @@ AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/'
 
-BASE_URL = "https://surgicalm.com"
+# Add in prod 
+# BASE_URL = "https://surgicalm.com"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
